db07245a6bfc587c208daf65650c26fc74789c8d
prabodhkumar005 <prabodhkumar7254@gmail.com>
2024-08-24 10:31:54 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Button from "./Button"
+import Input from "./Input"
+import Postcard from "./Postcard"
+import PostForm from "./post-form.jsx/PostForm";
+import AuthLayout from "./AuthLayout"
+import Login from "./Login";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Button,
+    Input,
+    Postcard,
+    PostForm,
+    Select,
+    AuthLayout,
+    Login
+
+}
